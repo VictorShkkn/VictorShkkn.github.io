edf40c0c31de836510e77767c1861c3399a32a73
VictorShkkn <48925464+VictorShkkn@users.noreply.github.com>
2021-08-20 15:24:26 +0200

    Add files via upload

--- page-frontent/russian сook/script.js
+++ page-frontent/russian сook/script.js
@@ -1,5 +1,6 @@
 let burger = document.querySelector(".header__burger");
 let nav = document.querySelector(".nav");
+let navLink = document.querySelectorAll(".nav__link");
 
 function openCloseBurger() {
     document.querySelector(".header-conainer").classList.toggle("active");
@@ -11,12 +12,31 @@ function openCloseBurger() {
     if (nav.classList.contains("active")) {
         burger.innerHTML = '<img src="img/close.svg" alt="">';
     }
-   
+
     else  {
         burger.innerHTML = '<img src="img/burger.svg" alt="">';
     }
-    
 }
 
-
 burger.onclick = openCloseBurger;
+
+window.addEventListener("resize", widthScreen);
+
+for (i of navLink) {
+    i.addEventListener("click", openCloseBurger);
+}
+
+widthScreen();
+function widthScreen() {
+    if (window.innerWidth <= 992) {
+        for (i of navLink) {
+            i.addEventListener("click", openCloseBurger);
+        }
+        
+    }
+    else {
+        for (i of navLink) {
+            i.removeEventListener("click", openCloseBurger);
+        }
+    }
+}
